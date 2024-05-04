ca0d2e95bcee416a7e23aeee38f770b5f908cde7
vaifai <vaifaipandey1996@gmail.com>
2024-05-04 19:31:37 +0530

    Extracting the correct URL

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import * as fs from 'fs';
 import bencode from 'bencode'
-const torrent = bencode.decode(fs.readFileSync('puppy.torrent'));
+const torrent = bencode.decode(fs.readFileSync('puppy.torrent'), 'utf-8');
 console.log(torrent.announce.toString('utf8'));
